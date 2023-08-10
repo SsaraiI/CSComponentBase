f28ba97efbd863eb3b9565d705f2926d44df9653
Protist <98732781+SsaraiI@users.noreply.github.com>
2023-08-11 01:11:42 +0700

    MuayThaiCompetition ex3.3
    MuayThaiCompetition program show update score from user's input to two reporter (Buakaow and Somluck). Only "Enter" for exit program.

---
+++ MuayThaiCompetition/src/footballcompetitionmt/FootballCompetitionMT.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package footballcompetitionmt;
+import java.util.Scanner;
+
+/**
+ *
+ * @author iarsk
+ */
+public class FootballCompetitionMT {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Thai 1-0 UAE input
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        Buakaw buakaw = new Buakaw();
+        Somluck somluck = new Somluck(); 
+        HeadQuater s = new HeadQuater();
+        s.register(buakaw);
+        s.register(somluck);
+        String line;
+        do {
+            System.out.print("enter Score ");
+            line = sn.nextLine();
+            if(line.length() > 0)
+                s.setScoreLog(line);
+        }while (line.length() > 0);
+        sn.close();
+    }
+    
+}
