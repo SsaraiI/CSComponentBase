fe718025ef5216249b8a6ee3a56966677a680e56
Protist <98732781+SsaraiI@users.noreply.github.com>
2023-08-10 23:36:44 +0700

    custmailwfactory ex3.1
    custmailwofactory to custmailwfactory finish

---
+++ custmailwofactory/src/customermailapplication/CustomerFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customermailapplication;
+
+/**
+ *
+ * @author iarsk
+ */
+public class CustomerFactory {
+    public static Customer createCustomer(String customerType) {
+        Customer customer = null;
+        switch(customerType) {
+            case "Regular":
+                customer = new RegularCustomer();
+                break;
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
+        }
+        return customer;
+    }
+}
