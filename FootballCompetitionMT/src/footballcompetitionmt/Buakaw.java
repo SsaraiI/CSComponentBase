4cb30d49d68513c73359592454903b553ad30322
Protist <98732781+SsaraiI@users.noreply.github.com>
2023-08-11 01:22:26 +0700

    FootballCompetitionMT ex3.3
    FootballCompetitionMT program show update score from user's input to two reporter (Buakaow and Somluck). Only "Enter" for exit program.

---
+++ FootballCompetitionMT/src/footballcompetitionmt/Buakaw.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package footballcompetitionmt;
+
+/**
+ *
+ * @author iarsk
+ */
+public class Buakaw implements ObserverMT {
+    @Override
+    public void update(Source o) {
+        System.out.println("(Buakaw) live result :  " + ((HeadQuater)o).getScoreLog());
+    }
+}
---
+++ FootballCompetitionMT/src/footballcompetitionmt/ObserverMT.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package footballcompetitionmt;
+
+/**
+ *
+ * @author iarsk
+ */
+public interface ObserverMT {
+        public void update(Source o);
+}
