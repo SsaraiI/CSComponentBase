062ed45f8f73576cc3c02e2398aad3c23c1bb367
Protist <98732781+SsaraiI@users.noreply.github.com>
2023-08-18 14:21:58 +0700

    Football competition using Event ex5.2
    enter to exit
    register and check remove register
    
    Example
    Enter Score Thai 1-1 UAE <-- input
    live result:Thai 1-1 UAE <--output subscriber1
    live result:Thai 1-1 UAE <--output subscriber2
    Enter Score Thai 1-2 UAE
    live result:Thai 1-2 UAE
    live result:Thai 1-2 UAE
    Enter Score 
    live result:check 0-0 check : remove register

---
+++ FootballUsingEvent/src/footballusingevent/Subscriber.java
@@ -0,0 +1,10 @@
+package footballusingevent;
+
+public class Subscriber implements ScoreListener{
+
+    @Override
+    public void scoreChange(ScoreEvent evt) {
+       System.out.println("live result:" + evt.getScoreLine());
+    }
+    
+}
